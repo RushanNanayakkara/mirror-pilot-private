b7146a113da1c0a2d00a5235ef717e59aedeb44a
rushannanayakkara <rushan@wso2.com>
2026-08-04 22:19:57 +0530

    Hotfix: uppercase the info prefix for a customer log parser

--- src/Logger.java
+++ src/Logger.java
@@ -1,6 +1,6 @@
 public class Logger {
     public void info(String msg) {
-        System.out.println("[info] " + msg);
+        System.out.println("[INFO] " + msg);
     }
     public void warn(String msg) {
         System.out.println("[warn] " + msg);
