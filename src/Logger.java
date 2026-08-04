92efe2475a7ee77fb5ec2cde041e0bfe89347f29
rushannanayakkara <rushan@wso2.com>
2026-08-04 22:23:56 +0530

    Resolve: take the public spelling

--- src/Logger.java
+++ src/Logger.java
@@ -1,10 +1,6 @@
 public class Logger {
     public void info(String msg) {
-<<<<<<< HEAD
-        System.out.println("[INFO] " + msg);
-=======
         System.out.println("[information] " + msg);
->>>>>>> 0a87d0e (Spell out the info log prefix (#8))
     }
     public void warn(String msg) {
         System.out.println("[warn] " + msg);
